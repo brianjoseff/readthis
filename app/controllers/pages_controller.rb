31f57ac93eead0558f31eb8bc4fa2b66de08da79
tibet000000 <Brian.D.P.Joseff@dartmouth.edu>
2014-08-18 08:24:06 -0700

    index controller fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
   def index
     @luminary = User.first
-    @luminarys_book_list = @luminary.book_lists.first
+    unless @luminary.nil?
+      @luminarys_book_list = @luminary.book_lists.first
+    end
   end
 
   def about
