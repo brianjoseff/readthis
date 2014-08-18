820b4b4b42782f30bb6bd07dad24fb834bf0529d
tibet000000 <Brian.D.P.Joseff@dartmouth.edu>
2014-08-18 11:35:45 -0700

    protect credentials

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -3,11 +3,7 @@ class Asset < ActiveRecord::Base
   
   has_attached_file :image, :styles => { :normal => "100%",:small => "100 x100>",:medium => "200x200>", :thumb => "50x50>", :mini => "25x25>" },
                             :storage => :s3,
-                            :s3_credentials => {
-                              bucket: 'readthis-dev',
-                              access_key_id: 'AKIAIYILNTDZMLVW7SWA',
-                              secret_access_key: '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'
-                            },
+                            :s3_credentials => "#{Rails.root}/config/s3.yml", 
                             :path => "/:attachment/:id/:style/:filename"
                             
   # :s3_credentials => "#{Rails.root}/config/s3.yml", 
